d098ddec83b4a31d50526172409e158fe1ad2d45
Vicente <vicenteferrer_01@hotmail.com>
2025-10-30 13:22:10 -0300

    Collage styled

--- src/components/Collage.tsx
+++ src/components/Collage.tsx
@@ -1,23 +1,27 @@
-import { useCollage } from "@hooks/useCollage";
 import { extractPhotoUUID } from "@lib/utils";
+
+import { useCollage } from "@hooks/useCollage";
+
+import "./Collage.css";
 
 export default function Collage() {
   const { images } = useCollage();
 
   return (
-    <section>
+    <article className="collage">
       {images.map((image) => {
         const photoID = extractPhotoUUID(image.name);
 
         return (
-          <img
-            key={photoID}
-            src={`/api/photos/${photoID}?maxWidth=100`}
-            alt={image.authorAttributions[0]?.displayName || "Place photo"}
-            loading="lazy"
-          />
+          <div key={photoID} className="collage-item">
+            <img
+              src={`/api/photos/${photoID}?maxWidth=800`}
+              alt={image.authorAttributions[0]?.displayName || "Place photo"}
+              loading="lazy"
+            />
+          </div>
         );
       })}
-    </section>
+    </article>
   );
 }
